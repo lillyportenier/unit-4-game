a0d485f5b500671ce55174e964f368c894735b55
laportenier <lilly.portenier@gmail.com>
2019-10-01 14:50:15 -0400

    followed steps in crystal game to get layout done

---
+++ javascript/game.js
@@ -0,0 +1,37 @@
+var targetNumber = 53;
+var counter = 0;
+
+
+$("#number-to-guess").text(targetNumber);
+
+var counter = 0;
+
+var numberOptions = [10, 5, 3, 7];
+
+for (var i = 0; i < numberOptions.length; i++) {
+    var imagePuppy = $("<img>");
+    imagePuppy.addClass("puppy-image");
+    imagePuppy.attr("src", "http://3.bp.blogspot.com/-JrlY7xnioKs/Ufg0PepAc7I/AAAAAAAADlM/JREyEbUXDsw/s1600/IMG_8988.JPG");
+    imagePuppy.attr("data-puppyvalue", numberOptions[i]);
+    $("#puppys").append(imagePuppy);
+}
+
+
+
+$(".puppy-image").on("click", function () {
+    
+    var puppyValue = ($(this).attr("data-puppyvalue"));
+    puppyValue = parseInt(puppyValue);
+
+    counter += puppyValue;
+
+
+    alert("Your new score is " + counter);
+
+    if (counter === targetNumber) {
+        alert("You WIN!!!");
+    }
+    else if (counter >= targetNumber) {
+        alert("You lost!!");
+    }
+});
